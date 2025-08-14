af47630d2ad54c34f4ca0e445cd73d76c6626612
nicolas balao <nicolasbalao.pro@gmail.com>
2025-08-14 12:43:21 +0200

    feat(base64): Add decode logic + test

--- Base64_encoder_decoder/src/main.zig
+++ Base64_encoder_decoder/src/main.zig
@@ -67,6 +67,54 @@ const Base64 = struct {
 
         return out;
     }
+
+    pub fn _char_index(self: Base64, char: u8) u8 {
+        if (char == '=') {
+            return 64;
+        }
+
+        var index: u8 = 0;
+
+        for (0..63) |i| {
+            if (self._char_at(i) == char) {
+                break;
+            }
+            index += 1;
+        }
+
+        return index;
+    }
+
+    pub fn decode(self: Base64, input: []const u8, allocator: std.mem.Allocator) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+
+        const n_output = try calcDecodeLenght(input);
+        var output = try allocator.alloc(u8, n_output);
+
+        var count: u8 = 0;
+        var iout: u64 = 0;
+        var buff = [4]u8{ 0, 0, 0, 0 };
+
+        for (0..input.len) |i| {
+            buff[count] = self._char_index(input[i]);
+            count += 1;
+
+            if (count == 4) {
+                output[iout] = (buff[0] << 2) + (buff[1] >> 4);
+                if (buff[2] != 64) {
+                    output[iout + 1] = (buff[1] << 4) + (buff[2] >> 2);
+                }
+                if (buff[3] != 64) {
+                    output[iout + 2] = (buff[2] << 6) + buff[3];
+                }
+                iout += 3;
+                count = 0;
+            }
+        }
+        return output[0..iout - 1];
+    }
 };
 
 fn calcEncodeLenght(input: []const u8) !usize {
@@ -88,7 +136,7 @@ fn calcDecodeLenght(input: []const u8) !usize {
 
     var result: usize = n_groups * 4;
 
-    var i = 0;
+    var i: usize = 0;
 
     while (i > 0) : (i -= 1) {
         if (input[i] == '=') {
@@ -105,26 +153,48 @@ pub fn main() !void {
     const base64 = Base64.init();
 
     const input = "zig";
+    const etext = "VGVzdGluZyBzb21lIG1vcmUgc3R1ZmY=";
     var memory_buffer: [1000]u8 = undefined;
 
     var fba = std.heap.FixedBufferAllocator.init(&memory_buffer);
     const allocator = fba.allocator();
 
-    const output = try base64.encode(input, allocator);
+    const encoded_output = try base64.encode(input, allocator);
+    const decoded_ouput = try base64.decode(etext, allocator);
 
-    std.debug.print("Base 64 {s} : {s} \n\n", .{ input, output });
+    std.debug.print("decoded: '{s}'\n", .{decoded_ouput});
+    std.debug.print("len: {}, bytes: {any}\n", .{ decoded_ouput.len, decoded_ouput });
 
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    std.debug.print("Base 64 encoded {s} : {s} \n\n", .{ input, encoded_output });
+    std.debug.print("Base 64 decoded {s} : {s} \n\n", .{ etext, decoded_ouput });
+}
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+test "should encode base64" {
+    const base64 = Base64.init();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const input = "zig";
+    const expected = "emln";
 
-    try bw.flush(); // Don't forget to flush!
+    var memory_buffer: [1000]u8 = undefined;
+
+    var fba = std.heap.FixedBufferAllocator.init(&memory_buffer);
+    const allocator = fba.allocator();
+    const encoded = try base64.encode(input, allocator);
+
+    try std.testing.expectEqualStrings(expected, encoded);
+}
+
+test "should decode base64" {
+    const base64 = Base64.init();
+
+    const input = "VGVzdGluZyBzb21lIG1vcmUgc3R1ZmY=";
+    const expected = "Testing some more stuff";
+
+    var memory_buffer: [1000]u8 = undefined;
+
+    var fba = std.heap.FixedBufferAllocator.init(&memory_buffer);
+    const allocator = fba.allocator();
+    const decoded = try base64.decode(input, allocator);
+
+    try std.testing.expectEqualStrings(expected, decoded);
 }
